97d56ad574642f54d3316e083fca1adbd3f51362
David Reina <davidereina@hotmail.com>
2023-10-05 15:21:39 -0500

    Example of searching a character in a string entered by the user, and adicionally an example of concantenation of two variables with text

---
+++ Challenge2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Challenge2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string here: ");
+            string inputString = Console.ReadLine();
+            Console.Write("Enter a character to search: ");
+            char characterToSearch = Console.ReadLine()[0];
+            int indexFound = inputString.IndexOf(characterToSearch);
+            Console.WriteLine($"The matching index for the character searched is {indexFound}");
+
+            //Concatenation
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            string lastname = Console.ReadLine();
+            string fullname = name + " " + lastname;
+            Console.WriteLine(fullname);
+        }
+    }
+}
